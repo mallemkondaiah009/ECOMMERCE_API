90b10d1a41ddf2ef2e5c61758446a33777b27a23
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-11-07 20:47:20 +0530

    Update User model field lengths for username and email

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=10)
-    email = models.EmailField(max_length=15)
+    username = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
     password = models.CharField(max_length=10)
 
     def __str__(self):
