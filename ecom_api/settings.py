533672989aec6f847d7b29d4de46066af147bcd7
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-11-07 20:48:04 +0530

    Add frontend app to INSTALLED_APPS and configure template directory path

--- ecom_api/settings.py
+++ ecom_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'store',
+    'frontend',
     #'payments',
     'rest_framework',
     'rest_framework_simplejwt'
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'ecom_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
