a560b46cf6a2bfa7c661c9fea9218223e80b42f4
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-11-16 11:26:45 +0530

    Update URL patterns to include cart and product view paths

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import register_view, login_view, profile_view, home_view
+from .views import register_view, login_view, profile_view, home_view, cart_view, productView_view
 
 urlpatterns = [
+    path('', home_view, name='home_view'),
     path('register/', register_view, name='register_view'),
     path('login/', login_view, name='login_view'),
     path('profile/', profile_view, name='profile_view'),
-    path('', home_view, name='home_view'),
+    path('cart/', cart_view ),
+    path('view-product/', productView_view)
+    
 ]
