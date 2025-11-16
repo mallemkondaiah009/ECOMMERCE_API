8de1ebf0b64d987555edc0f25968ebdbc4796c8e
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-11-16 11:28:27 +0530

    Update URL patterns to include display product and random products endpoints

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from .views import ProductSearch, AllProducts, AddToCart, UpdateProduct
+from .views import ProductSearch, AllProducts, AddToCart, DisplayProductWithId, RandomProducts
 
 
 urlpatterns = [
     path('api/search/<str:search_name>/',ProductSearch.as_view()),
     path('api/products/', AllProducts.as_view()),
     path('api/add-to-cart/', AddToCart.as_view()),
-    path('api/cart-item-remove/<int:pk>/', UpdateProduct.as_view()),
+    path('api/product/<int:pk>/', DisplayProductWithId.as_view()),
+    path('api/random-products/',RandomProducts.as_view() )
 ]
