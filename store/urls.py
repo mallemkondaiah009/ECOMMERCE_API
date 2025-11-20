6005571a26d4b197fd4078d8e1f39508b507a581
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-11-20 12:08:39 +0530

    add remove cart item url

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import ProductSearch, AllProducts, AddToCart, DisplayProductWithId, RandomProducts
+from .views import ProductSearch, AllProducts, AddToCart, DisplayProductWithId, RandomProducts, DeleteCartItemView
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api/products/', AllProducts.as_view()),
     path('api/add-to-cart/', AddToCart.as_view()),
     path('api/product/<int:pk>/', DisplayProductWithId.as_view()),
-    path('api/random-products/',RandomProducts.as_view() )
+    path('api/random-products/',RandomProducts.as_view() ),
+    path('api/cart/<int:pk>/', DeleteCartItemView.as_view()),
 ]
