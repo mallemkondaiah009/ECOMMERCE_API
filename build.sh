62022f44ebf181619dd53a4a09a25fd404e3b4db
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-11-24 07:53:08 +0530

    Update build.sh to use Gunicorn for ASGI application; modify requirements.txt

--- build.sh
+++ build.sh
@@ -12,4 +12,16 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Step 4: Start the Server (Optional)
-uvicorn ecom_api.asgi:application
+
+exec gunicorn ecom_api.asgi:application \
+    --name ecom_api \
+    --bind 0.0.0.0:${PORT:-8000} \
+    --workers ${GUNICORN_WORKERS:-4} \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --log-level=${GUNICORN_LOG_LEVEL:-info} \
+    --access-logfile - \
+    --error-logfile - \
+    --max-requests 10000 \
+    --max-requests-jitter 500 \
+    --timeout 120 \
+    --keep-alive 5
